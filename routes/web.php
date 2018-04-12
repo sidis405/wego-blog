451dc4d2d2258fd6d2146c9e07e4645c349534ac
Sidrit Trandafili <forge405@gmail.com>
2018-04-12 19:28:29 +0200

    entities creation

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// RESOURCE - REST
+// index - /posts - GET - lista tutti i task
+// create - /posts/create - GET - form creazione task
+// store - /posts - POST - crea task
+// show - /posts/{post} - GET - singolo task
+// edit - /posts/{post}/edit - GET - form modifica task
+// update - /posts/{post} - PATCH - modifica task
+// destroy - /posts/{post} - DELETE - cancella
